afc803e2e26fade2481cd2e0f312a9e402521f6c
Prince Vuha <princevuha@yahoo.com>
2025-11-02 17:32:07 +0000

    resolve error with running from app-example to app

--- prodev-mobile-app-0x00/components/ui/collapsible.tsx
+++ prodev-mobile-app-0x00/components/ui/collapsible.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren, useState } from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 
-import { Colors } from '@/app-example/constants/Colors';
+import { Colors } from '@/constants/Colors';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { IconSymbol } from '@/components/ui/icon-symbol';
